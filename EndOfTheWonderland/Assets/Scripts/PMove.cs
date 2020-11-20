558a5adcf29fe1c432d8ebec34182395e691782b
hjm0423 <32761043+hjm0423@users.noreply.github.com>
2020-11-20 11:28:53 +0900

    이미지수정
    이미지수정

---
+++ EndOfTheWonderland/Assets/Scripts/PMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PMove : MonoBehaviour
+{
+    Transform tr;
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if (Input.GetMouseButton(0))
+        {
+            tr.Translate(Vector3.up * Time.deltaTime * 10, Space.Self);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
